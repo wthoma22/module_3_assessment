10a86f44fc1aad7d7a2877a81f79d92754a5cb69
William Thomas <wthoma390@gmail.com>
2017-07-26 11:12:44 -0600

    WIP faraday call to view

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,7 +2,7 @@ class StoresController < ApplicationController
 
   def index
     result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}")
-    raw_results = JSON.parse(result.body, symbolize_names: true)[:stores]
+    raw_results = JSON.parse(result.body, symbolize_names: true)
     @results = raw_results.map do |store|
       Store.new(store)
     end
