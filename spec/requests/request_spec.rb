215a87025f10c0196e2d7f0fa66a3ec8d0b369d2
William Thomas <wthoma390@gmail.com>
2017-07-26 09:44:03 -0600

    adds fourth request spec for creating an item

--- spec/requests/request_spec.rb
+++ spec/requests/request_spec.rb
@@ -43,31 +43,27 @@ RSpec.describe "Items API endpoints" do
   scenario "deletes an item" do
     expect(Item.count).to eq(3)
 
-    deletes api_v1_item(@item1)
+    delete api_v1_item(@item1)
 
     expect(Item.count).to eq(2)
     expect(response).to be_success
   end
-end
 
-### 1. Create an API
-#
-# For this challenge clone [Storedom](https://github.com/turingschool-examples/storedom).
-#
-# We need an API for the application that can both read and write data. Start by focusing on functionality for items. All of this should happen in a dedicated, versioned controller.
-#
-# When I send a GET request to `/api/v1/items`
-# I receive a 200 JSON response containing all items
-# And each item has an id, name, description, and image_url but not the created_at or updated_at
-#
-# When I send a GET request to `/api/v1/items/1`
-# I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
-#
-# When I send a DELETE request to `/api/v1/items/1`
-# I receive a 204 JSON response if the record is successfully deleted
-#
-# When I send a POST request to `/api/v1/items` with a name, description, and image_url
-# I receive a 201 JSON  response if the record is successfully created
-# And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
-#
-# * Verify that your non-GET requests work using Postman or curl. (Hint: `ActionController::API`). Why doesn't the default `ApplicationController` support POST and PUT requests?
+  scenario "creates an item" do
+    params = { name: "Widget", description: "Just Lovely", image_url: "google.com" }
+    post api_v1_items_path, params: { item: params }
+
+    expect(response).to be_success
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item[:name]).to eq("Widget")
+    expect(item[:description]).to eq("Just Lovely")
+    expect(item[:image_url]).to eq("google.com")
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a String
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a String
+    expect(item).to have_key(:image_url)
+    expect(item[:image_url]).to be_a String
+  end
+end
