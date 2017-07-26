07fbda73f446fe4249a048ebf67ea8b6f8faa044
William Thomas <wthoma390@gmail.com>
2017-07-26 10:58:56 -0600

    sets up stores controller to pass to view

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,6 +2,10 @@ class StoresController < ApplicationController
 
   def index
     result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}")
-    binding.pry
+    raw_results = JSON.parse(result.body, symbolize_names: true)[:stores]
+    @results = raw_results.map do |store|
+      Store.new(store)
+      binding.pry
+    end
   end
 end
