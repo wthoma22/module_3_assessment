562c6d358eb17a7eda4f77c3115115dfa7e85b66
William Thomas <wthoma390@gmail.com>
2017-07-26 11:52:31 -0600

    hacky version to populate views

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,9 +2,11 @@ class StoresController < ApplicationController
 
   def index
     result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}")
-    raw_results = JSON.parse(result.body, symbolize_names: true)
-    @results = raw_results.map do |store|
-      Store.new(store)
+    array = []
+    raw_results = JSON.parse(result.body, symbolize_names: true)[:stores]
+    raw_results.each do |store|
+      array << Store.new(store)
     end
+    @stores = array
   end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,9 +1,9 @@
 class Store
   attr_reader :name, :storeId, :store_Type
 
-  def initialize(raw_store)
-    @name = raw_store["name"]
-    @store_Id = raw_store["storeId"]
-    @store_Type = raw_store["store_Type"]
+  def initialize(store)
+    @name = store[:name]
+    @storeId = store[:storeId]
+    @storeType = store[:storeType]
   end
 end
