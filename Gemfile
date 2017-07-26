1cc3a20f2c8e150d66e5d95a04f96bf3ed729d57
William Thomas <wthoma390@gmail.com>
2017-07-26 09:28:06 -0600

    adds factory girl to gemfile, adds request specs, creates first request spec for getting all items

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ group :development, :test do
   gem "launchy"
   gem "shoulda-matchers"
   gem "database_cleaner"
+  gem 'factory_girl_rails'
 end
 
 group :test do
---
+++ spec/requests/request_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe "Items API endpoints" do
+  before do
+    item1 = FactoryGirl.create_list(:item, 3)
+  end
+
+  scenario "it returns all items" do
+    get 'api/v1/items'
+
+    expect(response).to be_success
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    item = json_response.first
+
+    expect(json_response.count).to eq(3)
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a String
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a String
+    expect(item).to have_key(:image_url)
+    expect(item[:image_url]).to be_a String
+
+  end
+end
+
+### 1. Create an API
+#
+# For this challenge clone [Storedom](https://github.com/turingschool-examples/storedom).
+#
+# We need an API for the application that can both read and write data. Start by focusing on functionality for items. All of this should happen in a dedicated, versioned controller.
+#
+# When I send a GET request to `/api/v1/items`
+# I receive a 200 JSON response containing all items
+# And each item has an id, name, description, and image_url but not the created_at or updated_at
+#
+# When I send a GET request to `/api/v1/items/1`
+# I receive a 200 JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+#
+# When I send a DELETE request to `/api/v1/items/1`
+# I receive a 204 JSON response if the record is successfully deleted
+#
+# When I send a POST request to `/api/v1/items` with a name, description, and image_url
+# I receive a 201 JSON  response if the record is successfully created
+# And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+#
+# * Verify that your non-GET requests work using Postman or curl. (Hint: `ActionController::API`). Why doesn't the default `ApplicationController` support POST and PUT requests?
