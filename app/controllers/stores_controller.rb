34a08c6dca11d8bf82fe5ff46962bc7496895387
William Thomas <wthoma390@gmail.com>
2017-07-26 12:08:12 -0600

    adds encoder to faraday call, this was the issue

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,12 +1,18 @@
 class StoresController < ApplicationController
 
   def index
-    result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}")
-    array = []
-    raw_results = JSON.parse(result.body, symbolize_names: true)[:stores]
-    raw_results.each do |store|
-      array << Store.new(store)
-    end
-    @stores = array
+    service = BestBuyService.new(@zip)
+    @stores = service.stores
+    @total = service.total
   end
+
+  # def index
+  #   result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}")
+  #   array = []
+  #   raw_results = JSON.parse(result.body, symbolize_names: true)[:stores]
+  #   raw_results.each do |store|
+  #     array << Store.new(store)
+  #   end
+  #   @stores = array
+  # end
 end
--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,28 +1,32 @@
-# class BestBuyService
-#
-#   def initialize(zip)
-#     @zip = zip
-#   end
-# 
-#   def connection
-#     @conn ||= Faraday.new("https://api.bestbuy.com/v1/stores")
-#   end
-#
-#   def response
-#     connection.get(default_api_path).body
-#   end
-#
-#   def default_api_path
-#     "(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}"
-#   end
-#
-#   def json_stores
-#     JSON.parse(response, symbolize_names: true)
-#   end
-#
-#   def stores
-#     json_stores.map do |raw_store|
-#       Store.new(raw_store)
-#     end
-#   end
-# end
+class BestBuyService
+
+  def initialize(zip)
+    @zip = zip
+  end
+
+  def conn
+    Faraday.new(:url => 'https://api.bestbuy.com/v1/') do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger                  
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def response
+    connection.get(default_api_path).body
+  end
+
+  def default_api_path
+    "(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}"
+  end
+
+  def json_stores
+    JSON.parse(response, symbolize_names: true)
+  end
+
+  def stores
+    json_stores.map do |raw_store|
+      Store.new(raw_store)
+    end
+  end
+end
