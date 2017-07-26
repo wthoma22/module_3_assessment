e1d9f14c0afcfa0a2878dfbfda3f201c1272f504
William Thomas <wthoma390@gmail.com>
2017-07-26 10:56:59 -0600

    faraday issues resolved, result.body is passing through in pry

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
 
   def index
+    result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV["BEST-BUY-API"]}")
     binding.pry
-    result = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV['BEST-BUY-API']}")
   end
 end
