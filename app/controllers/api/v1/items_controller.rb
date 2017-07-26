31a200d8969dee6b66fed8e268316a183dd4791a
William Thomas <wthoma390@gmail.com>
2017-07-26 09:55:58 -0600

    adds api/v1 controller methods for item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,13 +1,36 @@
 class API::V1::ItemsController < ApplicationController
+  before_action :set_before, except: [:index, :create]
 
   def index
+    render json: Item.all
+  end
 
+  def show
+    render json: @item
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save!
+      render json: item
+    end
+  end
+
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
   end
 
 private
 
-  def set_before
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
+  end
 
+  def set_before
+    @item = Item.find(params[:id])
   end
 
 end
