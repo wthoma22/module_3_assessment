1f0fbb0a8d9753bfaa0d0576ea5771a5332023eb
William Thomas <wthoma390@gmail.com>
2017-07-31 13:08:46 -0600

    update first half of search spec without additional functionality

--- spec/features/search_for_stores_spec.rb
+++ spec/features/search_for_stores_spec.rb
@@ -11,23 +11,20 @@ RSpec.describe "Visitor" do
         click_on "Search"
 
         expect(current_path).to eq(search_path)
-        expect(page).to have_content("Stores within 25 miles of 80202")
         expect(page).to have_content("17 Total Stores")
-        expect(page).to have_css(".stores", count: 10)
-        within ".stores" do
-          expect(page).to have_link("#")
-          expect(page).to have_content("#")
-          expect(page).to have_content("#")
-          expect(page).to have_content("#")
-          expect(page).to have_content("#")
+        within ".search-results" do
+          expect(page).to have_selector(".store", count: 10)
+          within ".store-1" do
+            expect(page).to have_selector(".long-name")
+            expect(page).to have_selector(".city")
+            expect(page).to have_selector(".distance")
+            expect(page).to have_selector(".phone-number")
+            expect(page).to have_selector(".store-type")
         end
       end
     end
   end
 end
-
-### 2. Consume Best Buy's API
-#
 # As a user
 # When I visit "/"
 # And I fill in a search box with "80202" and click "search"
@@ -36,3 +33,17 @@ end
 # And I should see a message that says "17 Total Stores"
 # And I should see exactly 10 results (There are 17 stores within 25 miles. We want to display 10 on this page and 7 on the next. Get the first page to work first from start to finish and worry about pagination later.)
 # And I should see the long name, city, distance, phone number and store type for each of the 10 results
+#
+# (Additional Functionality)
+#
+# And I should see pagination links below the search results for the number of pages (2 in this case)
+# And `1` is my current page but isn't a clickable link
+# And `2` is a clickable link
+# When I click `2`
+# Then I should be taken to the next page of search results
+# Then my current path should be "/search" (ignoring params)
+# And in the params I should see `page=2`
+# And I should see stores within 25 miles
+# And I should see a message that says "17 Total Stores"
+# And I should see the next 6 results
+# And I should see the long name, city, distance, phone number and store type for each of the next 6 results
