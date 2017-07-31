91bf3a0aa5fdc81730cb8b383546b13fff8b9938
William Thomas <wthoma390@gmail.com>
2017-07-31 14:08:49 -0600

    sets up pagination but receiveing no method error

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,7 +3,7 @@ class StoresController < ApplicationController
   def index
     location = params[:find]
     @stores = Store.find_by_zip(location)
-    # @stores_count = stores.count
-    # @stores = stores.paginate(page: params[:page], per_page: 10)
+    @stores_count = @stores.count
+    @store = @stores.paginate(page: params[:page], per_page: 10)
   end
 end
